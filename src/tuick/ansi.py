74788e655e3e9af47e9736df6d2f6398da4ef758
David Allouche <david@ddaa.net>
2025-11-21 13:19:35 +0100

    🚧 🐛 strip_ansi supports more codes, NEED TEST
    Example string for test:
    'src/tuick/console.py:77: \x1b[1m\x1b[31merror:\x1b(B\x1b[m Name \x1b(B\x1b[m\x1b[1m"undefined_variable_here"\x1b(B\x1b[m is not defined  \x1b(B\x1b[m\x1b[33m[name-defined]\x1b(B\x1b[m\n'

--- src/tuick/ansi.py
+++ src/tuick/ansi.py
@@ -5,14 +5,20 @@ import re
 # ANSI escape code regex pattern
 ANSI_REGEX = re.compile(
     r"""
-    \x1b  # ESC
+    \x1b              # ESC character (0x1b)
     (?:
-        [@-Z\\-_]  # Fe sequences: ESC + single byte (ESC M, ESC 7, etc.)
+        \[            # CSI - Control Sequence Introducer
+        [0-?]*        # Parameter bytes (optional): 0-9 : ; < = > ?
+        [ -/]*        # Intermediate bytes (optional): space through /
+        [@-~]         # Final byte: the actual command (m, H, J, K, etc.)
         |
-        \[         # CSI sequences: ESC [ params intermediates final
-        [0-?]*     # Parameter bytes (optional)
-        [ -/]*     # Intermediate bytes (optional)
-        [@-~]      # Final byte
+        \(            # SCS G0 - Select Character Set
+        [B0UK]        # B=ASCII, 0=line drawing, U=null, K=user
+        |
+        \)            # SCS G1 - Select Character Set
+        [B0UK]        # Same options as G0
+        |
+        [@-Z\\-_]     # Fe sequences: two-byte escapes
     )
     """,
     re.VERBOSE,
